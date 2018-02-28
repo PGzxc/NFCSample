99eecd8d8e5d0fbab13c6e406fa98bde1ea4b5f5
pgzxc <827489398@qq.com>
2018-02-28 23:17:42 +0800

    Initial commit

---
+++ app/src/main/java/com/pgzxc/nfcsample/InstalledApplicationListActivity.java
@@ -0,0 +1,51 @@
+package com.pgzxc.nfcsample;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by admin on 2018/2/28.
+ */
+
+public class InstalledApplicationListActivity extends ListActivity implements
+        AdapterView.OnItemClickListener {
+    private List<String> mPackages = new ArrayList<String>();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        PackageManager packageManager = getPackageManager();
+        List<PackageInfo> packageInfos = packageManager
+                .getInstalledPackages(PackageManager.GET_ACTIVITIES);
+        for (PackageInfo packageInfo : packageInfos) {
+
+            mPackages.add(packageInfo.applicationInfo.loadLabel(packageManager)
+                    + "\n" + packageInfo.packageName);
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1,
+                mPackages);
+        setListAdapter(arrayAdapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+                            long id) {
+        Intent intent = new Intent();
+        intent.putExtra("package_name", mPackages.get(position));
+        setResult(1, intent);
+        finish();
+    }
+}
